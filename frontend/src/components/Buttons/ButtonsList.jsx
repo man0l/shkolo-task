590bbc3f95fb0a0b35dbb04aedec513fede72a3a
Manol Trendafilov <manol@camplight.dev>
2025-03-25 18:12:01 +0200

    add empty layout

--- frontend/src/components/Buttons/ButtonsList.jsx
+++ frontend/src/components/Buttons/ButtonsList.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import { getButtons } from '../../services/buttonsSerivce';
+import { HiMiniPlus } from "react-icons/hi2";
+
 
 const ButtonsList = () => {
     const [buttons, setButtons] = useState([]);
@@ -8,13 +10,19 @@ const ButtonsList = () => {
         getButtons().then(setButtons);
     }, []);
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-2xl font-bold mb-4">Buttons List</h1>
-            <ul className="flex flex-col gap-4">
-                {buttons.map((button) => (
-                    <li key={button.id} className="bg-gray-100 p-4 rounded-md size-32">{button.title}</li>
+        <div className="flex flex-col w-full p-4 md:p-6">
+            <div className="text-2xl font-bold mb-6 md:mb-8 text-center md:text-left">SHKOLO</div>
+            <div className="w-full max-w-6xl mx-auto">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 sm:gap-4 md:gap-6">
+                {Array.from({ length: 9 }).map((_, index) => (
+                    <div
+                        key={index}
+                        className="bg-gray-200 rounded-md items-center justify-center flex w-full aspect-2/1"
+                    > <HiMiniPlus />
+                    </div>
                 ))}
-            </ul>
+                </div>
+            </div>
         </div>
     )
 }
