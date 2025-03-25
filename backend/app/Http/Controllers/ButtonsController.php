fe7a819b938bfa4157240bf085d10416d5abe8c3
Manol Trendafilov <manol@camplight.dev>
2025-03-25 11:58:14 +0200

    initial setup model migration request and contorller

---
+++ backend/app/Http/Controllers/ButtonsController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Button;
+use App\Http\Requests\StoreButtonRequest;
+use App\Http\Requests\UpdateButtonRequest;
+
+class ButtonsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $buttons = Button::all();
+        return response()->json($buttons);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreButtonRequest $request)
+    {
+        $button = Button::create($request->all());
+        return response()->json($button, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Button $button)
+    {
+        return response()->json($button);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateButtonRequest $request, Button $button)
+    {
+        $button->update($request->all());
+        return response()->json($button);
+    }
+}
